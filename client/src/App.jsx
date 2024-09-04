eda36a3dc884c51f954cb2d6c1cb3e3de6b34811
Prashant gupta <prashantguptaofficial007@gmail.com>
2024-09-04 20:31:56 +0530

    Implementation auth logic

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,32 +12,55 @@ import Home from "./pages/shopping-view/Home"
 import Listing from "./pages/shopping-view/Listing"
 import Checkout from "./pages/shopping-view/Checkout"
 import Account from "./pages/shopping-view/Account"
+import CheckAuth from "./components/common/CheckAuth"
+import UnAuth from "./pages/un-auth/UnAuth"
 
-function App() {
+function App() { 
+
+  const isAuthenticated = true;
+  const user = {
+    name: 'Prashant',
+    role: 'admin'
+  }
 
   return (
     <div className="flex flex-col w-full overflow-hidden">
 
-      <Routes >
+      <Routes>
 
-        <Route path="/auth" element={<AuthLayout />}>
+        <Route path="/auth" element={
+          <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+            <AuthLayout />
+          </CheckAuth>
+        }>
           <Route path="login" element={<Login />}/>
           <Route path="register" element={<Register />}/>
         </Route>
 
-        <Route path="/admin" element={<AdminLayout />}>
+        <Route path="/admin" element={
+          <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+            <AdminLayout />
+          </CheckAuth>
+        }>
           <Route path="dashboard" element={<Dashboard />}/>
           <Route path="product" element={<Produt />}/>
           <Route path="orders" element={<Orders />}/>
         </Route>
 
-        <Route path="/shop" element={<ShopingLayout />}>
+        <Route path="/shop" element={
+          <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+            <ShopingLayout />
+          </CheckAuth>
+        }>
           <Route path="home" element={<Home />}/>
           <Route path="listing" element={<Listing />}/>
           <Route path="checkout" element={<Checkout />}/>
           <Route path="account" element={<Account />}/>
         </Route>
+
         <Route path="*" element={<NotFound />}/>
+
+        <Route path="/unauth" element={<UnAuth />}/>
 
       </Routes>
     </div>
---
+++ client/src/pages/un-auth/UnAuth.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UnAuth = () => {
+  return (
+    <div>You dont access to this page</div>
+  )
+}
+
+export default UnAuth
